c9454bdcf73a59dc7711b8b0bad2bb1b4b43d896
Eric Boehs <eric@boehs.com>
2019-12-20 17:36:53 -0600

    Clean start -- remove all files

--- config/spring.rb
+++
@@ -1,8 +0,0 @@
-%w(
-  .ruby-version
-  .rbenv-vars
-  tmp/restart.txt
-  tmp/caching-dev.txt
-  .env
-  .env.local
-).each { |path| Spring.watch(path) }
--- db/seeds.rb
+++
@@ -1,2 +0,0 @@
-User.where(name: 'John Doe', email: 'john@example.com').first_or_create password: 'password'
-User.where(name: 'Jane Doe', email: 'jane@example.com').first_or_create password: 'password'
